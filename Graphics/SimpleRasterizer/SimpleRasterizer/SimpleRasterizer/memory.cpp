c4099e06810a45b20dc37562fbabcebc9b7e54d9
Zeeseungyun <36136016+Zeeseungyun@users.noreply.github.com>
2021-04-02 18:38:12 +0900

    asdf
    asdf

--- Graphics/SimpleRasterizer/SimpleRasterizer/SimpleRasterizer/memory.cpp
+++ Graphics/SimpleRasterizer/SimpleRasterizer/SimpleRasterizer/memory.cpp
@@ -1,1 +1,31 @@
 #include "memory.h"
+namespace zee {
+namespace detail {
+	std::size_t reference_counter<thread_safety::nosafe>::increase() noexcept {
+		return ++count;
+	}
+
+	std::size_t reference_counter<thread_safety::nosafe>::decrease() noexcept {
+		if (count) {
+			--count;
+		}
+		return count;
+	}
+
+	std::size_t reference_counter<thread_safety::safe>::increase() noexcept {
+		return ++count;
+	}
+
+	std::size_t reference_counter<thread_safety::safe>::decrease() noexcept {
+		if (count) {
+			--count;
+		}
+		return count;
+	}
+
+	std::size_t reference_counter<thread_safety::safe>::get() const {
+		return count;
+	}
+}//namespace detail 
+
+}//namespace zee 
--- Graphics/SimpleRasterizer/SimpleRasterizer/SimpleRasterizer/memory.h
+++ Graphics/SimpleRasterizer/SimpleRasterizer/SimpleRasterizer/memory.h
@@ -2,6 +2,7 @@
 #include "base.h"
 #include <memory>
 #include <mutex>
+#include <atomic>
 
 namespace zee {
 	enum class thread_safety : uint8 {
@@ -9,7 +10,10 @@ namespace zee {
 		safe,
 	};
 
-	class reference_counter {
+namespace detail {
+	template<thread_safety Safety> struct reference_counter;
+
+	template<> class reference_counter<thread_safety::nosafe> {
 	public:
 		std::size_t increase() noexcept;
 		std::size_t decrease() noexcept;
@@ -17,22 +21,30 @@ namespace zee {
 
 	private:
 		std::size_t count;
-		mutable std::mutex mtx;
 	};
+
+	template<> class reference_counter<thread_safety::safe> {
+	public:
+		std::size_t increase() noexcept;
+		std::size_t decrease() noexcept;
+		std::size_t get() const;
+
+	private:
+		std::atomic_size_t count;
+	};
+}//namespace detail 
 
 	template<typename T>
 	class shared_object_base {
 	public:
 		shared_object_base() : object(nullptr) { }
-		~shared_object_base() {
-		}
+		~shared_object_base() { }
 
 	private:
-		T* object;
+		T object;
 	};
 
-	template<typename T, thread_safety safety>
-	class shared_object;
+	template<typename T, thread_safety Safety = thread_safety::nosafe> class shared_object;
 
 	template<typename T>
 	class shared_object<T, thread_safety::nosafe> {
